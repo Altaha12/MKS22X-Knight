3a8c0a22a8cdaee48d1802063341e77eb6caa214
Ali Taha <alitaha.1187@gmail.com>
2019-02-26 23:55:01 -0500

    Created solve with exceptions and created many helpers to bread down problem, so far can count number of possibles at a given square, and determine if possible

--- KnightBoard.java
+++ KnightBoard.java
@@ -26,32 +26,43 @@ public class KnightBoard{
       }
     return brd;
     }
-  public static void main(String[] args) {
-    System.out.println("Testing the Constructor");
-
-    KnightBoard A=new KnightBoard(1,2);
-    System.out.println("This is a 1 by 2 board");
-    System.out.print(A);
-
-    KnightBoard B=new KnightBoard(3,4);
-    System.out.println("This is a 3 by 4 board");
-    System.out.print(B);
-
-    KnightBoard C=new KnightBoard(8,8);
-    System.out.println("This is a 8 by 8 board");
-    System.out.print(C);
-    int x=0;
-    for(int row=0;row<C.board.length;row++){
-      for(int col=0;col<C.board[0].length;col++){
-        C.board[row][col]=x;
-        x+=1;
+  public boolean solve(int startingRow, int startingCol) throws IllegalStateException,IllegalArgumentException{
+    //checks Illegal State Exception
+    for(int i=0;i<board.length;i++){
+      for(int j=0;j<board[0].length;j++){
+        if(board[i][j]!=0)throw new IllegalStateException();
       }
-
     }
-    System.out.println("This is a 8 by 8 board With single and double digit numbers");
-    System.out.print(C);
-
+    if(startingRow<0||startingRow>=board.length||startingCol<0||startingCol>=board[0].length)throw new IllegalArgumentException();
+    return solveH(startingRow,startingCol,1);
   }
+  //starts at row and column and goes down tree of all possibles in order of least moves possible there
+  private boolean solveH(int row, int col, int move){
+    return true;
+  }
+  //gives a list of possibles sorted by number of paths
+  private int[][] possibles(int row, int col){
+    return board;
+  }
+  //gives number of possibles from a given square
+  private int possiblesCount(int row, int col){
+    int[][] moves= new int[][]{{1,2},{1,-2},{-1,2},{-1,-2},{2,1},{2,-1},{-2,1},{-2,-1}};
+    int count=0;
+    for(int[] i : moves ){
+      if(possible(row+i[0], col+i[1]))count+=1;
+    }
+    return count;
+  }
+  //returns if a given square is possible
+  private boolean possible(int row,int col){
+    return row>=0&&col>=0&&row<board.length&&col<board[0].length&&board[row][col]==0;
+  }
+  public static void main(String[] args) {
+    KnightBoard A = new KnightBoard(8,8);
+    A.board[4][2]=1;
+    System.out.println(A.possiblesCount(0,0));
+    System.out.println(A.possiblesCount(0,2));
+    System.out.println(A.possiblesCount(3,4));
 
-
+}
 }
